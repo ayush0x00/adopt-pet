a0e6e540dbde5cfe2c72f47c77f268a0e3b72446
Cipher2472 <ayushtiwari5747462@gmail.com>
2020-11-29 01:25:36 +0530

    Carousel Implementation

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,18 @@
 import React from "react";
-const Pet = ({ name, animal, breed }) => {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
   return (
-    <div>
-      <h2>{name}</h2>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} alt={name}>
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal}-${location}`}</h2>
+      </div>
+    </a>
   );
-};
-
-export default Pet;
+}
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,29 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useDropdown from "./useDropdown";
-import { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Delhi,India");
+  const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+    //console.log(animals);
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds: apiBreed }) => {
+      const breedString = apiBreed.map(({ name }) => name);
+      //setBreed(breedString);
+      setBreeds(breedString);
+    });
+  }, [animal, setBreeds, setBreed]);
+
   return (
     <div className="search-params">
-      <h1>{location}</h1>
-      <h1>{animal}</h1>
-      <label htmlFor="location">
-        location
-        <input
-          id="location"
-          value={location}
-          placeholder="Location"
-          onChange={(e) => setLocation(e.target.value)}
-          onBlur={(e) => setLocation(e.target.value)}
-        />
-      </label>
-      <AnimalDropdown />
-      <BreedDropdown />
-      <button>Search</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <h1>{location}</h1>
+        <h1>{animal}</h1>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+            onBlur={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
     </div>
   );
 };
